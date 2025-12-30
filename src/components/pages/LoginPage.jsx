c45313d782de5b93903c459ab601a7ae6f6918aa
BenaliAiEngineer <abdelhafid.benali@proton.me>
2025-12-30 21:35:56 +0100

    ✨F01:LOGIN>Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+  //State
+  const [inputValue,setInputValue] = useState('');
+  //Comportements
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+  const handleChange = (event)=>{
+    setInputValue(event.target.value)
+  }
+
+  //Render
   return (
     <div>
-      
+      <h1>Bienvenue chez nous!</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+       <input value ={inputValue} type="text" placeholder='Entrez votre prénom ...' required onChange={handleChange}/>
+       <button>Accédez à votre espace </button>
+      </form>
     </div>
   )
 }
