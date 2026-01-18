447b3a7364a8f562255eaa4911a2c34d2352f7da
BenaliAiEngineer <abdelhafid.benali@proton.me>
2026-01-18 02:00:37 +0100

    ✨F02:APP>instal routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import "./App.css"
 import LoginPage from "./components/pages/login/LoginPage";
+import { Routes,Route } from 'react-router-dom';
+import OrderPage from './components/pages/login/order/OrderPage';
+import ErrorPage from './components/pages/login/error/ErrorPage'
+
 
 
 
 function App() {
   
 
-  return <LoginPage/>
-   
+  return (
+         <Routes>
+           <Route path="/" element={<LoginPage/>}/>
+           <Route path = "/order/:userName" element = {<OrderPage/>}/>
+           <Route path = "*" element = {<ErrorPage/>}/>
+          
+         </Routes>
+  )
+  
+  
 }
 
 export default App
