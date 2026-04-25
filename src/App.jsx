d559d9cca12b15014ad34471d3a9c635d59ebbef
Abdelhafid <abdelhafid.benali@proton.me>
2026-04-25 08:50:24 +0200

    Correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import { useState } from "react"
+import LoginPage from "./components/pages/login/LoginPage"
 function App() {
-  
+   
 
-  return (
-    <h1>Abdelhafid</h1>
-  )
+  return <LoginPage />
+    
 }
 
 export default App
