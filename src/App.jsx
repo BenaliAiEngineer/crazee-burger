f3a4a4c3dd71e7a10e2512fc0ff3f246df791c7e
BenaliAiEngineer <abdelhafid.benali@proton.me>
2026-04-29 01:42:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import LoginPage from "./components/pages/login/LoginPage"
-import OrderPage from "./components/pages/login/order/OrderPage"
-import ErrorPage from "./components/pages/login/error/ErrorPage"
+import OrderPage from "./components/pages/order/OrderPage"
+import ErrorPage from "./components/pages/error/ErrorPage"
 import {Routes ,Route} from "react-router-dom"
 function App() {
    
@@ -9,7 +9,7 @@ function App() {
   return (
           <Routes>
             <Route path="/" element ={<LoginPage/>}/>
-            <Route path = "/order/:id" element ={<OrderPage/>}/>
+            <Route path = "/order/:username" element ={<OrderPage/>}/>
             <Route path ="*" element = {<ErrorPage/>}/>
           </Routes>
            
